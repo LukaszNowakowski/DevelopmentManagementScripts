519a3c817f91fe60231e34b7eec55383081bc3ac
LukaszNowakowski-it <bitbucket@lnow.pl>
2020-07-13 10:21:48 +0200

    ReferentialData and SwaggerUI don't have their databases.

--- DeployApplicationsDatabases.ps1
+++ DeployApplicationsDatabases.ps1
@@ -10,6 +10,4 @@ function DeployDatabase
 DeployDatabase "C:\AzureDevOpsWorkspaces\Applications\Integration\Agdf_CommercialOffers\BuildUtils\database_deploy.ps1"
 DeployDatabase "C:\AzureDevOpsWorkspaces\Applications\Integration\Agdf_Documents\BuildUtils\database_deploy.ps1"
 DeployDatabase "C:\AzureDevOpsWorkspaces\Applications\Integration\Agdf_InsuranceCompanies\BuildUtils\database_deploy.ps1"
-DeployDatabase "C:\AzureDevOpsWorkspaces\Applications\Integration\Agdf_ReferentialData\BuildUtils\database_deploy.ps1"
 DeployDatabase "C:\AzureDevOpsWorkspaces\Applications\Integration\Agdf_SecurityAudit\BuildUtils\database_deploy.ps1"
-DeployDatabase "C:\AzureDevOpsWorkspaces\Applications\Integration\Agdf_SwaggerUI\BuildUtils\database_deploy.ps1"
