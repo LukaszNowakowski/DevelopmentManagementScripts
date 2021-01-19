1472e4928da1dde1a8c6d70c3d205c7b7d18573f
Lukasz Nowakowski <lukasz.nowakowski@axadirect-solutions.pl>
2021-01-19 14:52:56 +0100

    Run all job engines - there are different job runner types.

--- RunProcessors.ps1
+++ RunProcessors.ps1
@@ -4,17 +4,66 @@ param (
 
 function StartProcessor {
 	param (
-		[String]$Directory,
-		[String]$Application
+		[String]$RunnerName,
+		[String]$Executable
 	)
 	
+	$Directory = "C:\Runners\$RunnerName"
 	$app = "cmd"
-	$parameters = @("/C"; "cd $Directory && $Application")
-	Start-Process $app $parameters
+	$parameters = @("/C"; "cd $Directory && $Executable")
+	Write-Host $parameters
+	# Start-Process $app $parameters
 }
 
-Clear-Host
+function CreateCopy{
+	param (
+		[String]$SourceDirectory,
+		[String]$Name
+	)
+	
+	New-Item "C:\Runners\$Name" -Type Directory
+	Copy-Item "$SourceDirectory\*" "C:\Runners\$Name\"
+}
 
-StartProcessor "$BranchDirectory\Sandpiper.DocumentGeneration\Sandpiper.DocumentGeneration.SelfHosted\bin\Debug" "Sandpiper.DocumentGeneration.SelfHosted.exe"
-StartProcessor "$BranchDirectory\Sandpiper.JobEngine\Sandpiper.JobEngine.SelfHosted\bin\Debug" "Sandpiper.JobEngine.SelfHosted.exe"
-StartProcessor "$BranchDirectory\Sandpiper.JobEngine\Sandpiper.SyncJobEngine.SelfHosted\bin\Debug" "Sandpiper.SyncJobEngine.SelfHosted.exe"
+function ChangeJobRunnerId{
+	param (
+		[String]$RunnerName,
+		[String]$RunnerId
+	)
+	
+	$config = [Xml](Get-Content "C:\Runners\$RunnerName\Sandpiper.JobEngine.SelfHosted.exe.config")
+	$jobRunnerId = $config.configuration.appSettings.add | where { $_.key -eq "BatchPollingServiceConfiguration.JobRunnerId" }
+	$jobRunnerId.value = $RunnerId
+	$config.Save("C:\Runners\$RunnerName\Sandpiper.JobEngine.SelfHosted.exe.config")
+}
+
+Write-Host "Clearing runners"
+Remove-Item "C:\Runners\*" -Recurse
+
+Write-Host "Preparing and running document generation"
+CreateCopy "$BranchDirectory\Sandpiper.DocumentGeneration\Sandpiper.DocumentGeneration.SelfHosted\bin\Debug" "DocumentGeneration"
+StartProcessor "DocumentGeneration" "Sandpiper.DocumentGeneration.SelfHosted.exe"
+
+Write-Host "Preparing and running JobEngine1"
+CreateCopy "$BranchDirectory\Sandpiper.JobEngine\Sandpiper.JobEngine.SelfHosted\bin\Debug" "JobEngine1"
+ChangeJobRunnerId "JobEngine1" "1"
+StartProcessor "JobEngine1" "Sandpiper.JobEngine.SelfHosted.exe"
+
+Write-Host "Preparing and running JobEngine2"
+CreateCopy "$BranchDirectory\Sandpiper.JobEngine\Sandpiper.JobEngine.SelfHosted\bin\Debug" "JobEngine2"
+ChangeJobRunnerId "JobEngine2" "2"
+StartProcessor "JobEngine2" "Sandpiper.JobEngine.SelfHosted.exe"
+
+Write-Host "Preparing and running JobEngine3"
+CreateCopy "$BranchDirectory\Sandpiper.JobEngine\Sandpiper.JobEngine.SelfHosted\bin\Debug" "JobEngine3"
+ChangeJobRunnerId "JobEngine3" "3"
+StartProcessor "JobEngine3" "Sandpiper.JobEngine.SelfHosted.exe"
+
+Write-Host "Preparing and running JobEngine4"
+CreateCopy "$BranchDirectory\Sandpiper.JobEngine\Sandpiper.JobEngine.SelfHosted\bin\Debug" "JobEngine4"
+ChangeJobRunnerId "JobEngine4" "4"
+StartProcessor "JobEngine4" "Sandpiper.JobEngine.SelfHosted.exe"
+
+Write-Host "Preparing and running SyncJobEngine"
+CreateCopy "$BranchDirectory\Sandpiper.JobEngine\Sandpiper.SyncJobEngine.SelfHosted\bin\Debug" "SyncJobEngine"
+StartProcessor "SyncJobEngine" "Sandpiper.SyncJobEngine.SelfHosted.exe"
